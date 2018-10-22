afb088b662d738c8d5b9c05e100a7eda1add2f65
Sania <saniaspence90210@gmail.com>
2018-10-22 14:04:48 -0500

    Update Exercise 3: Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -7,18 +7,27 @@ window.onload = function(){
 		boundary.classList.add("youlose");
 	};
 };*/
+lose = false;
+
 window.onload = function(){
 
 	const boundaries = document.getElementById("maze").querySelectorAll(".boundary");
+	const end = document.getElementById("end");
 
 	for (let i=0;i<boundaries.length;i++){
 	boundaries[i].addEventListener("mouseover", function(){
-		
+		lose = true;
 		for (i=0;i<boundaries.length;i++){
 			boundaries[i].classList.add("youlose")
 		}
 
 	});
 	}
+
+	end.addEventListener("mouseover", function(){
+		if (lose == false){
+			alert("You Win!");
+		}
+	});
 
 };
