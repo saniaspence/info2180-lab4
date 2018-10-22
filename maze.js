e6b7264c65bc70dca6dd5534c051c1d54cba860f
Sania <saniaspence90210@gmail.com>
2018-10-22 13:28:25 -0500

    Update Exercise 2: All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,3 +1,4 @@
+/*Exercise 1: Single boundary turns red
 window.onload = function(){
 	
 	const boundary = document.getElementById("boundary1");
@@ -5,4 +6,19 @@ window.onload = function(){
 	boundary.onmouseover = function(){
 		boundary.classList.add("youlose");
 	};
+};*/
+window.onload = function(){
+
+	const boundaries = document.getElementById("maze").querySelectorAll(".boundary");
+
+	for (let i=0;i<boundaries.length;i++){
+	boundaries[i].addEventListener("mouseover", function(){
+		
+		for (i=0;i<boundaries.length;i++){
+			boundaries[i].classList.add("youlose")
+		}
+
+	});
+	}
+
 };
